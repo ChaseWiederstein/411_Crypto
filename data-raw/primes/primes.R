e25dfdfdc37edc19bcd0f05f28a0b58f0397880a
RobWiederstein <khuon68@gmail.com>
2022-10-14 16:43:31 -0400

    encrypt rsa number

--- data-raw/primes/primes.R
+++ data-raw/primes/primes.R
@@ -3,5 +3,5 @@ df <- readr::read_table(file = file, skip = 3, col_names = F, n_max = 100)
 df <- df[, -11]
 df1 <- df |> tidyr::pivot_longer(cols = X1:X10)
 primes <- df1$value
-m <- matrix(primes[1:400], ncol = 10, byrow = TRUE)
+m <- matrix(primes, ncol = 10, byrow = TRUE)
 saveRDS(m, file = "./data/primes.rds")
