3d224e9e75f487c1e2361517215de0aea5872661
RobWiederstein <khuon68@gmail.com>
2022-10-31 09:02:08 -0400

    omit non-ascii characters

--- data-raw/acronym/clean_nist_acronym.R
+++ data-raw/acronym/clean_nist_acronym.R
@@ -1,9 +1,12 @@
+# url: https://nvlpubs.nist.gov/nistpubs/SpecialPublications/NIST.SP.800-175Br1.pdf
 df <- read.csv("./data-raw/acronym/nist_acronyms.csv", na.strings = "", header = F)
-install.packages("dplyr")
-library(dplyr)
 df1 <- 
   df |> 
   na.omit(df) |> 
   dplyr::rename(acronym = V1,
                 defined = V2)
-saveRDS(df1, file = "./data/acronyms.rds")
+row.names(df1) <- NULL
+df1[2, 2] <- "American National Standard"
+df1[14, 2] <- "Diffie-Hellman algorithm"
+df1[36, 2] <- "Menezes-Qu-Vanstone algorith; specified in SP 800-56A15"
+saveRDS(df1, file = "./data/acronyms.rds") #ascii = T
